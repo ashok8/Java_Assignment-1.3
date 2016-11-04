4e8c8e58c443713e5d0b15cc3abe30cbc20b5e8b
ashok8 <ashok8@users.noreply.github.com>
2016-11-04 18:57:21 +0530

    Add files via upload

---
+++ swap2varwo3.java
@@ -0,0 +1,14 @@
+public class swap2varwo3 {
+
+	public static void main(String[] args) {
+		int a =9, b= 7;
+		System.out.printf("a =%d and b= %d", a, b);
+		b = a + b;
+		a = b - a;
+		b = b - a;
+		System.out.printf("\nNow a =%d and b= %d", a, b);
+		
+
+	}
+
+}
